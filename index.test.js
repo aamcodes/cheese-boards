040e7853dfa5f3ed59f9050273c2d8d4c82f1094
aamcodes <aaronmendezcodes@gmail.com>
2023-03-03 13:31:00 -0500

    Added tests for boards and cheeses to ensure the associatians work properly.

--- index.test.js
+++ index.test.js
@@ -10,7 +10,7 @@ describe('User, Board, Cheese Models', () => {
 	});
 
 	it('should create a new user', async () => {
-		let newUser = await new User({
+		let newUser = await User.create({
 			name: 'Aaron',
 			email: 'aaron@gmail.com',
 		});
@@ -19,7 +19,7 @@ describe('User, Board, Cheese Models', () => {
 	});
 
 	it('should create a new board', async () => {
-		let newBoard = await new Board({
+		let newBoard = await Board.create({
 			type: 'Chess',
 			description: 'A Chess Board',
 			rating: 900,
@@ -30,7 +30,7 @@ describe('User, Board, Cheese Models', () => {
 	});
 
 	it('should create a new cheese', async () => {
-		let newCheese = await new Cheese({
+		let newCheese = await Cheese.create({
 			title: 'Cheddar',
 			description: 'Amazing Cheesy flavor loved by all',
 		});
@@ -38,5 +38,60 @@ describe('User, Board, Cheese Models', () => {
 		expect(newCheese.dataValues.description).toBe(
 			'Amazing Cheesy flavor loved by all'
 		);
+	});
+});
+
+describe('One To Many and Many To Many Associations', () => {
+	beforeEach(async () => {
+		// the 'sync' method will create tables based on the model class
+		// by setting 'force:true' the tables are recreated each time the
+		// test suite is run
+		await sequelize.sync({ force: true });
+	});
+
+	it('user and boards associatiate properly', async () => {
+		let newUser = await User.create({
+			name: 'Aaron',
+			email: 'aaron@gmail.com',
+		});
+		let newBoard = await Board.create({
+			type: 'Chess',
+			description: 'A Chess Board',
+			rating: 900,
+		});
+		await newUser.addBoard(newBoard.dataValues.id);
+		let data = await Board.findAll({
+			include: [{ model: User }],
+		});
+		expect(data[0].dataValues.user.dataValues.name).toBe('Aaron');
+		expect(data[0].dataValues.user.dataValues.email).toBe(
+			'aaron@gmail.com'
+		);
+	});
+
+	it('boards and cheeses associate properly', async () => {
+		let newBoard = await Board.create({
+			type: 'Chess',
+			description: 'A Chess Board',
+			rating: 900,
+		});
+		let newCheese = await Cheese.create({
+			title: 'Cheddar',
+			description: 'Amazing Cheesy flavor loved by all',
+		});
+		await newBoard.addCheese(newCheese.dataValues.id);
+		let data = await Board.findAll({
+			include: [{ model: Cheese }],
+		});
+		let data2 = await Cheese.findAll({
+			include: [{ model: Board }],
+		});
+
+		expect(data[0].dataValues.cheeses.length).toBe(1);
+		expect(data2[0].dataValues.boards[0].dataValues.type).toBe('Chess');
+		expect(data2[0].dataValues.boards[0].dataValues.description).toBe(
+			'A Chess Board'
+		);
+		expect(data2[0].dataValues.boards[0].dataValues.rating).toBe(900);
 	});
 });
