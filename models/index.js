67c9fdf4867b64f50432193992d472811be46c5b
aamcodes <aaronmendezcodes@gmail.com>
2023-03-03 11:42:40 -0500

    Added Relationships

--- models/index.js
+++ models/index.js
@@ -2,6 +2,12 @@ const { User } = require('./User');
 const { Board } = require('./Board');
 const { Cheese } = require('./Cheese');
 
+Board.belongsTo(User);
+User.hasMany(Board);
+
+Cheese.belongsToMany(Board, { through: 'board_cheeses' });
+Board.belongsToMany(Cheese, { through: 'board_cheeses' });
+
 module.exports = {
 	User,
 	Board,
